e77abe2370e5a832d185e7015b30b6eb381d5358
Ben Campion <bencampion@users.noreply.github.com>
2016-04-03 17:33:45 +0100

    Updated tests

--- src/test/java/uk/recurse/acme/utils/AcmeUtilsTest.java
+++ src/test/java/uk/recurse/acme/utils/AcmeUtilsTest.java
@@ -1,30 +1,47 @@
 package uk.recurse.acme.utils;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
 
 public class AcmeUtilsTest {
 
     @Test
-    public void testSort() {
-        List<Integer> a = Arrays.asList(3, 5, -1, 2, 4, 0, -2, 1);
-        AcmeUtils.sort(a, 5d);
-        assertEquals(a, Arrays.asList(-2, -1, 0, 1, 2, 3, 4, 5));
-        
-        List<Integer> b = Collections.emptyList();
-        AcmeUtils.sort(b, 0d);
-        assertEquals(b, Collections.emptyList());
+    public void sort_unsortedList_mutatesToSortedList() {
+        List<Integer> list = Arrays.asList(3, 5, -1, 2, 4, 0, -2, 1);
+        AcmeUtils.sort(list, 10);
+        assertThat(list, is(Arrays.asList(-2, -1, 0, 1, 2, 3, 4, 5)));
+    }
+
+    @Test
+    public void sort_emptyList_doesNotMutateList() {
+        List<Integer> list = Collections.emptyList();
+        AcmeUtils.sort(list, 0);
+        assertThat(list, is(sameInstance(list)));
+    }
+
+    @Test
+    public void join_list_returnsStringJoinedWithSeparator() {
+        String joined = AcmeUtils.join(Arrays.asList(1, 2, 3), ",");
+        assertThat(joined, is("1,2,3"));
+    }
+
+    @Test
+    public void join_singletonList_returnsElementAsString() {
+        String joined = AcmeUtils.join(Collections.singleton("x"), ",");
+        assertThat(joined, is("x"));
     }
     
     @Test
-    public void testJoin() {
-        assertEquals(AcmeUtils.join(Collections.emptyList(), ","), "");
-        assertEquals(AcmeUtils.join(Collections.singleton("x"), ","), "x");
-        assertEquals(AcmeUtils.join(Arrays.asList(1, 2, 3), ","), "1,2,3");
+    public void join_emptyList_returnsEmptyString() {
+        String joined = AcmeUtils.join(Collections.emptyList(), ",");
+        assertThat(joined, is(""));
     }
 
 }
